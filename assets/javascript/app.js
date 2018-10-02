afee1b964cebf2b7ffb27b91f55f67f52eb7eec1
Frank Santana <frigsamo@gmail.com>
2018-10-02 16:05:59 -0400

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,15 +38,14 @@ $(function () {
                 url:  api + query  +   apiKey  +  limit,
                 method: "GET"
             }).then(function (response) {
-                console.log(response.data);
+                
                 response.data.forEach(function(element,index){
-                   // console.log(index);
+                   
                   url = element.images.fixed_height.url;
                   urlList.push(element.images.fixed_height.url);
                   urlStatic = element.images.fixed_height_still.url;
                   urlStaticList.push(element.images.fixed_height_still.url);
-                 console.log(url);
-                 console.log(urlStatic);
+                
                  imges = $("<img src=" + urlStatic + " id='"+index + "'" + ">");
                  $(".imagesPosition").append(imges);
                 
@@ -60,13 +59,12 @@ $(function () {
     var time = () => {
         setTimeout(function(){
             $("img").on("click",function(){
-                console.log($(this).attr('src'));
-                console.log($(this).attr("id"));
+               
                 if($(this).attr('src') == urlStaticList[$(this).attr('id')] ){
                     console.log("entro a estatic");
                  $(this).attr('src',urlList[$(this).attr('id')]);
               }else if($(this).attr('src') == urlList[$(this).attr('id')]){
-                  console.log("entro a la gif");
+                  
                  $(this).attr('src',urlStaticList[$(this).attr('id')]);
               }
             }) 
